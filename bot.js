711db30541751b97dda1faf5ac6fa8dcbfb3b80e
muzahidul190 <muzahidul190@gmail.com>
2018-03-05 12:57:55 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,8 +22,8 @@ bot.on('message', async message => {
     if(cmd == 'qwerty'){
         return message.reply('Are you on your keyboard?');
     }
-    if(cmd === 'bname'){
-        return message.channel.send('My name is');
+    if(cmd === 'clr'){
+        message.channel.bulkDelete(args[2]);
     }
     if(cmd == 'invite'){
         return message.channel.send('Invitatin link is https://discord.gg/crtrH5y');
