4f943581da9687ae49f042c1fbd4019366b06ad4
muzahidul190 <muzahidul190@gmail.com>
2018-03-05 06:35:48 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', assync() => {
+client.on('ready', async () => {
     console.log('&{bot.user.username} is online.');
     bot.user.setGame("Getting Constructed!!!");
 });
