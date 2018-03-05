22148b0e7eb1d964900c54eb41626b648a594f78
muzahidul190 <muzahidul190@gmail.com>
2018-03-05 06:52:20 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,18 @@
 const Discord = require('discord.js');
-const sgbot = new Discord.client();
+const client = new Discord.Client();
 
-sgbot.on('ready', async () => {
-    console.log('&{bot.user.username} is online.');
-    bot.user.setGame('Getting Constructed.');
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-sgbot.on('message', message => {
-    if (message.content === 'testn') {
-    	message.channel.send('I am alive.');
-  	}else if(message.content == 'qwerty'){
+client.on('message', message => {
+    if (message.content === 'teston') {
+    	message.reply('I am alive.');  	}else if(message.content == 'qwerty'){
         message.reply('Are you on your keyboard?');
     }else if(message.content == 'invite'){
-        message.channel.send('Invitatin link is https://discord.gg/crtrH5y');
+        message.reply('Invitatin link is https://discord.gg/crtrH5y');
     }
 });
 
 // THIS  MUST  BE  THIS  WAY
-sgbot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
