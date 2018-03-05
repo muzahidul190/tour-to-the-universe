3129c7e8fb0a898c0b9050c1c2b1d544311a149a
muzahidul190 <muzahidul190@gmail.com>
2018-03-05 12:52:07 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,13 +28,6 @@ bot.on('message', async message => {
     if(cmd == 'invite'){
         return message.channel.send('Invitatin link is https://discord.gg/crtrH5y');
     }
-    if(cmd === 'botinfo'){
-        let info = new Discord.RichEmbed()
-       .setDescription("Still Getting Constructed.")
-       .setColor("#ffa500")
-       .setField("Bot Name: ", bot.username);
-        return message.channel.send(info);
-    }
 });
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply('Oops, you dont have permission.');
@@ -42,7 +35,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.bulkDelete(args[0]);
 }
 module.exports.help = {
-    name: "clr"
+    name: "clear"
 }
 
 // THIS  MUST  BE  THIS  WAY
