ae0aa2ec8c06b4a2383886de95cefc829d5a8ed2
muzahidul190 <muzahidul190@gmail.com>
2018-03-09 21:43:37 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client({disableEveryone: true});
+const bot = new Discrd.Client({disableEveryone: true});
 
 bot.on('ready', async () => {
     console.log('I am ready!');
